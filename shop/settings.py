6ffcc37895dff303873247dec3d46432cd473465
Neha Kumari <55044120+Neha801@users.noreply.github.com>
2021-05-13 20:28:22 +0530

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -129,7 +129,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
